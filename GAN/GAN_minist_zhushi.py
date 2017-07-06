b775e9a48190ff7357d0382ee6ee44c78e712805
unknown <lixurui@upoa.com>
2017-07-06 10:00:37 +0800

    finetune  transfer

---
+++ GAN/GAN_minist_zhushi.py
@@ -0,0 +1,236 @@
+# -*- coding:utf-8 -*-
+'''
+DCGAN on MNIST using Keras
+Author: Rowel Atienza
+Project: https://github.com/roatienza/Deep-Learning-Experiments
+Dependencies: tensorflow 1.0 and keras 2.0
+Usage: python3 dcgan_mnist.py
+'''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+import numpy as np
+import time
+from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop
+
+
+import argparse
+
+import matplotlib.pyplot as plt
+FLAGS = None
+
+class ElapsedTimer(object):
+    def __init__(self):
+        self.start_time = time.time()
+    def elapsed(self,sec):
+        if sec < 60:
+            return str(sec) + " sec"
+        elif sec < (60 * 60):
+            return str(sec / 60) + " min"
+        else:
+            return str(sec / (60 * 60)) + " hr"
+    def elapsed_time(self):
+        print("Elapsed: %s " % self.elapsed(time.time() - self.start_time) )
+
+class DCGAN(object):
+    def __init__(self, img_rows=28, img_cols=28, channel=1):
+
+        self.img_rows = img_rows
+        self.img_cols = img_cols
+        self.channel = channel
+        self.D = None   # discriminator
+        self.G = None   # generator
+        self.AM = None  # adversarial model
+        self.DM = None  # discriminator model
+
+    # (W?F+2P)/S+1
+    def discriminator(self):  #卷积神经网络
+        if self.D:
+            return self.D
+        self.D = Sequential()
+        depth = 64
+        dropout = 0.4
+        # In: 28 x 28 x 1, depth = 1
+        # Out: 14 x 14 x 1, depth=64
+        input_shape = (self.img_rows, self.img_cols, self.channel)
+        self.D.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape,\
+            padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        self.D.add(Conv2D(depth*2, 5, strides=2, padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        self.D.add(Conv2D(depth*4, 5, strides=2, padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        self.D.add(Conv2D(depth*8, 5, strides=1, padding='same'))
+        self.D.add(LeakyReLU(alpha=0.2))
+        self.D.add(Dropout(dropout))
+
+        # Out: 1-dim probability
+        self.D.add(Flatten())
+        self.D.add(Dense(1))
+        self.D.add(Activation('sigmoid'))
+        self.D.summary()
+        return self.D
+
+    def generator(self):  #逆卷积神经网络
+        if self.G:
+            return self.G
+        self.G = Sequential()
+        dropout = 0.4
+        depth = 64+64+64+64
+        dim = 7
+        # In: 100
+        # Out: dim x dim x depth
+        self.G.add(Dense(dim*dim*depth, input_dim=100))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+        self.G.add(Reshape((dim, dim, depth)))
+        self.G.add(Dropout(dropout))
+
+        # In: dim x dim x depth
+        # Out: 2*dim x 2*dim x depth/2
+        self.G.add(UpSampling2D())
+        self.G.add(Conv2DTranspose(int(depth/2), 5, padding='same'))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+
+        self.G.add(UpSampling2D())
+        self.G.add(Conv2DTranspose(int(depth/4), 5, padding='same'))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+
+        self.G.add(Conv2DTranspose(int(depth/8), 5, padding='same'))
+        self.G.add(BatchNormalization(momentum=0.9))
+        self.G.add(Activation('relu'))
+
+        # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
+        self.G.add(Conv2DTranspose(1, 5, padding='same'))
+        self.G.add(Activation('sigmoid'))
+        self.G.summary()
+        return self.G
+
+    def discriminator_model(self): 
+        if self.DM:
+            return self.DM
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)
+        self.DM = Sequential()
+        self.DM.add(self.discriminator())
+        self.DM.compile(loss='binary_crossentropy', optimizer=optimizer,\
+            metrics=['accuracy'])
+        return self.DM
+
+    def adversarial_model(self):  #生成网络并没有一个独立的目标函数，而是 先将 G 拼接在 D 的上方，即G的输出作为 D 的输入
+        if self.AM:
+            return self.AM
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)
+        self.AM = Sequential()
+        self.AM.add(self.generator())
+        self.AM.add(self.discriminator())
+        self.AM.compile(loss='binary_crossentropy', optimizer=optimizer,\
+            metrics=['accuracy'])
+        return self.AM
+
+class MNIST_DCGAN(object):
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--data_dir', type=str, default='../mnist/data', help='Directory for storing data')
+        FLAGS = parser.parse_args()
+  
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channel = 1
+
+        self.x_train = input_data.read_data_sets(FLAGS.data_dir, one_hot=True).train.images
+        self.x_train = self.x_train.reshape(-1, self.img_rows,\
+        	self.img_cols, 1).astype(np.float32)
+
+        self.DCGAN = DCGAN()
+        self.discriminator =  self.DCGAN.discriminator_model()
+        self.adversarial = self.DCGAN.adversarial_model()
+        self.generator = self.DCGAN.generator()
+
+    def train(self, train_steps=1000, batch_size=256, save_interval=0):
+        noise_input = None
+        if save_interval>0:
+            noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
+        for i in range(train_steps):
+            images_train = self.x_train[np.random.randint(0,
+                self.x_train.shape[0], size=batch_size), :, :, :]
+            noise_gen = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            
+            ##1、重新计算假样本
+            images_fake = self.generator.predict(noise_gen)
+            x = np.concatenate((images_train, images_fake))
+            y = np.ones([2*batch_size, 1])
+             #上半部分是真实图片images_train，标记为1，下半部分是生成的图片，标记为0
+            y[batch_size:, :] = 0
+            
+            ##2、训练判别器（二分类） 为了最小化损失函数d_loss，使得判别器越来越准
+            d_loss = self.discriminator.train_on_batch(x, y)
+
+            ##3、训练生成网络，也是一个二分类神经网络，
+            #并将进入 G 的噪音样本标签全部改成'1'，目的就是要以假乱真
+            #同时固定 D 的参数  (这里没看懂，好像没有固定D的参数？应该要固定)
+            #为了最小化损失函数a_loss，此时就只能改变 G 的每一层权重，反复迭代后 G的生成能力因此得以改进。
+            y = np.ones([batch_size, 1])
+            noise_train = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])  #这句话好像可以不要，直接用这一轮已经生成的noise_gen训练即可，下一轮再重新生成，不过多生成一次好像更准
+            a_loss = self.adversarial.train_on_batch(noise_train, y)
+            
+            
+            
+            
+            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
+            print(log_mesg)
+            if save_interval>0:
+                if (i+1)%save_interval==0:
+                    self.plot_images(save2file=True, samples=noise_input.shape[0],\
+                        noise=noise_input, step=(i+1))
+
+    def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0):
+        filename = 'mnist.png'
+        if fake:
+            if noise is None:
+                noise = np.random.uniform(-1.0, 1.0, size=[samples, 100])
+            else:
+                filename = "mnist_%d.png" % step
+            images = self.generator.predict(noise)
+        else:
+            i = np.random.randint(0, self.x_train.shape[0], samples)
+            images = self.x_train[i, :, :, :]
+
+        plt.figure(figsize=(10,10))
+        for i in range(images.shape[0]):
+            plt.subplot(4, 4, i+1)
+            image = images[i, :, :, :]
+            image = np.reshape(image, [self.img_rows, self.img_cols])
+            plt.imshow(image, cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        if save2file:
+            plt.savefig(filename)
+            plt.close('all')
+        else:
+            plt.show()
+
+if __name__ == '__main__':
+    mnist_dcgan = MNIST_DCGAN()
+    timer = ElapsedTimer()
+    mnist_dcgan.train(train_steps=1000, batch_size=256, save_interval=500)
+    timer.elapsed_time()
+    mnist_dcgan.plot_images(fake=True)
+    mnist_dcgan.plot_images(fake=False, save2file=True)
