2b29487a7df19ad5708cba50262e35aaa4576662
unknown <lixurui@upoa.com>
2017-09-21 17:31:24 +0800

    对向量样本进行VAE  VAE_vecSamples.py

--- AutoEncoder/VAE_vecSamples.py
+++ AutoEncoder/VAE_vecSamples.py
@@ -11,6 +11,23 @@ from keras.layers import Input, Dense, Lambda
 from keras.models import Model  
 from keras import backend as K  
 from keras import objectives  
+ 
+ 
+from keras import regularizers
+
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import recall_score, precision_score
+ 
+#导入随机森林算法库
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression 
+from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import RandomizedSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.utils import shuffle 
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 #from keras.datasets import mnist  
 #from keras.utils.visualize_util import plot  
 import sys  
@@ -20,11 +37,41 @@ import os                          #python miscellaneous OS system tool
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
  
-batch_size = 100  
-original_dim = 784   #28*28  
+print "start"
+################################################################
+df_All = pd.read_csv("idx_new_08_del.csv", sep=',') 
+df_All = shuffle(df_All) 
+print df_All.shape[1]
+ 
+df_dummies =  df_All[["settle_tp","settle_cycle","block_id","trans_fwd_st","sms_dms_conv_in","cross_dist_in","tfr_in_in","trans_md","source_region_cd","dest_region_cd","cups_card_in","cups_sig_card_in","card_class","card_attr","acq_ins_tp","fwd_ins_tp","rcv_ins_tp","iss_ins_tp","acpt_ins_tp","resp_cd1","resp_cd3","cu_trans_st","sti_takeout_in","trans_id","trans_tp","trans_chnl","card_media","trans_curr_cd","conn_md","msg_tp","msg_tp_conv","trans_proc_cd","trans_proc_cd_conv","mchnt_tp","pos_entry_md_cd","pos_cond_cd","pos_cond_cd_conv","term_tp","rsn_cd","addn_pos_inf","iss_ds_settle_in","acq_ds_settle_in","upd_in","pri_cycle_no","disc_in","fwd_settle_conv_rt","fwd_settle_curr_cd","rcv_settle_curr_cd","acq_ins_id_cd_BK","acq_ins_id_cd_RG","fwd_ins_id_cd_BK","fwd_ins_id_cd_RG","rcv_ins_id_cd_BK","rcv_ins_id_cd_RG","iss_ins_id_cd_BK","iss_ins_id_cd_RG","acpt_ins_id_cd_BK","acpt_ins_id_cd_RG","settle_fwd_ins_id_cd_BK","settle_fwd_ins_id_cd_RG","settle_rcv_ins_id_cd_BK","settle_rcv_ins_id_cd_RG"]]
+ 
+
+df_X = pd.concat([df_All[["tfr_dt_tm","day_week","hour","trans_at","total_disc_at"]],df_dummies], axis=1).as_matrix()
+#df_X = df_All[["hour","trans_at","total_disc_at"]]
+print " df_X.shape", df_X.shape
+
+df_y = df_All["label"].as_matrix()
+
+x_train, x_test, y_train, y_test = train_test_split(df_X, df_y, test_size=0.2)
+
+sc = MinMaxScaler()   #这里如果用StandardScaler，那么会出现负数，导致算出来的KL距离 loss会有负数，
+
+#print X_train.loc[:1]
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+
+print " x_train.shape", x_train.shape,  "x_train.shape", x_test.shape 
+##################################################################
+
+
+  
+batch_size = 100
+original_dim = 67  
 latent_dim = 2  
-intermediate_dim = 256  
-nb_epoch = 50  
+intermediate_dim = 64  
+nb_epoch = 20  
 epsilon_std = 1.0  
   
 #my tips:encoding  
@@ -56,77 +103,27 @@ def vae_loss(x, x_decoded_mean):
     xent_loss = original_dim * objectives.binary_crossentropy(x, x_decoded_mean)  
     #my tips:see paper's appendix B    另一项 要求经过Q(z|x)估计出的隐变量分布接近于标准正态分布    由衡量两个分布的相似度，当然是大名鼎鼎的KL距离。二是近似后验与真实后验的KL散度，至于KL散度为何简化成代码中的形式，看论文《Auto-Encoding Variational Bayes》中的附录B有证明。
     kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)  
-    return xent_loss + kl_loss  
+    return K.mean(xent_loss + kl_loss) 
   
 vae = Model(x, x_decoded_mean)  
 vae.compile(optimizer='rmsprop', loss=vae_loss)  
   
-# train the VAE on MNIST digits  
-def load_data(path='mnist.npz'):
-    path = 'mnist.npz'
-    f = np.load(path)
-    x_train = f['x_train']
-    y_train = f['y_train']
-    x_test = f['x_test']
-    y_test = f['y_test']
-    f.close()
-    return (x_train, y_train), (x_test, y_test)
-                                                          
 
 
-(x_train, y_train), (x_test, y_test) = load_data()
-  
-x_train = x_train.astype('float32') / 255.  
-x_test = x_test.astype('float32') / 255.  
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))  
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))  
-  
 vae.fit(x_train, x_train,  
         shuffle=True,  
-        nb_epoch=nb_epoch,  
-        verbose=2,  
-        batch_size=batch_size,  
+        epochs=nb_epoch,  
+        verbose=2,   
+        batch_size=batch_size,
         validation_data=(x_test, x_test))  
   
 # build a model to project inputs on the latent space  
 encoder = Model(x, z_mean)  
-  
-# display a 2D plot of the digit classes in the latent space  
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
-plt.figure(figsize=(6, 6))  
-plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)  
-plt.colorbar()  
-plt.show()  
+ 
   
 # build a digit generator that can sample from the learned distribution  使用vae  跟普通自动编码器不一样，我们这里只需要 剪掉编码器部分，直接把正态分布样本送入解码器即可
 decoder_input = Input(shape=(latent_dim,))  
 _h_decoded = decoder_h(decoder_input)  
 _x_decoded_mean = decoder_mean(_h_decoded)  
 generator = Model(decoder_input, _x_decoded_mean)  
-  
-# display a 2D manifold of the digits  
-n = 15  # figure with 15x15 digits  
-digit_size = 28  
-figure = np.zeros((digit_size * n, digit_size * n))  
-# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian  
-# to produce values of the latent variables z, since the prior of the latent space is Gaussian  
-grid_x = norm.ppf(np.linspace(0.05, 0.95, n))  
-grid_y = norm.ppf(np.linspace(0.05, 0.95, n))  
-  
-for i, yi in enumerate(grid_x):  
-    for j, xi in enumerate(grid_y):  
-        z_sample = np.array([[xi, yi]])  
-        x_decoded = generator.predict(z_sample)  
-        digit = x_decoded[0].reshape(digit_size, digit_size)  
-        figure[i * digit_size: (i + 1) * digit_size,  
-               j * digit_size: (j + 1) * digit_size] = digit  
-  
-plt.figure(figsize=(10, 10))  
-plt.imshow(figure, cmap='Greys_r')  
-plt.show()  
-  
-#plot(vae,to_file='variational_autoencoder_vae.png',show_shapes=True)  
-#plot(encoder,to_file='variational_autoencoder_encoder.png',show_shapes=True)  
-#plot(generator,to_file='variational_autoencoder_generator.png',show_shapes=True)  
-  
  
